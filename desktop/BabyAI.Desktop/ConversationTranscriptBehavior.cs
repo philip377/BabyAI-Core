600052b0122be76dd5a64f531bb66b899a2cabd6
philip377 <32224901+philip377@users.noreply.github.com>
2026-08-13 17:50:02 +0300

    Update UI

--- desktop/BabyAI.Desktop/ConversationTranscriptBehavior.cs
+++ desktop/BabyAI.Desktop/ConversationTranscriptBehavior.cs
@@ -1,10 +1,10 @@
 using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
+using Microsoft.UI.Text;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
 using Windows.UI;
-using Windows.UI.Text;
 
 namespace BabyAI.Desktop;
 
